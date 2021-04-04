02b7ed81d9366c555f224283024abe100a34748d
sagrawal1234 <70326310+sagrawal1234@users.noreply.github.com>
2021-04-04 16:45:03 +0530

    Add files via upload

--- swappingFiles.py
+++ swappingFiles.py
@@ -1,34 +1,17 @@
-import os
+def swapFileData():
+    file1 = input("enter files name:- ")
+    file2 = input("enter files name:- ")
 
-file1 = input("Enter The First File Location You Want To Swap: ")
-file2 = input("Enter The Second File Location You Want To Swap: ")
+    with open(file1, 'r') as a:
+        data_a = a.read()
 
+	with open(file2, 'r') as b:
+        data_b = b.read()
 
-def swapFileData(f1, f2):
-    fl1 = open(f1, 'r')
-    fl2 = open(f2, 'r')
-    ln1 = fl1.readlines()
-    ln2 = fl2.readlines()
+    with open(file1, 'w') as a:
+        a.write(data_b)
 
-    fl1.close()
-    fl2.close()
+    with open(file2, 'w') as b:
+        b.write(data_a)
 
-    fl1 = open(f1, 'w')
-    fl2 = open(f2, 'w')
-
-    fl1.write('')
-    fl2.write('')
-
-    for i in ln1:
-        fl2.write(i)
-    for i in ln2:
-        fl1.write(i)
-
-
-exist1 = os.path.exists(file1)
-exist2 = os.path.exists(file2)
-
-if(exist1 == True and exist2 == True):
-    swapFileData(file1, file2)
-elif (exist1 == False or exist2 == False):
-    print("Give the file name correctly")
+swapFileData()
